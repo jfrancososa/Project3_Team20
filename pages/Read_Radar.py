75f34c405b5b1ca1a72413601b7d7e0cc51d4e41
Javier Perez <javierp621194@gmail.com>
2023-07-24 14:54:29 -0700

    Finished read radar base functionality with focus and break timers. Needs polishing up and better pdf reader.

--- pages/Read_Radar.py
+++ pages/Read_Radar.py
@@ -34,53 +34,150 @@ with pdf_col:
         st.write("No file uploaded yet")
 
 
-def set_time():
-    st.session_state.curr_time = st.session_state.hours * 3600 + st.session_state.minutes * 60
+def reset_focus_time():
+    st.session_state.focus_hours = 1
+    st.session_state.focus_minutes = 30
+    set_focus_time()
+    st.session_state.focus_start = False
+    st.session_state.focus_label = "Start"
 
 
-def bttn_toggle():
-    st.session_state.timer_start = not st.session_state.timer_start
-    if st.session_state.time_label == "Start":
-        st.session_state.time_label = "Stop"
+def reset_break_time():
+    st.session_state.break_hours = 0
+    st.session_state.break_minutes = 5
+    set_break_time()
+    st.session_state.break_start = False
+    st.session_state.break_label = "Start"
+
+
+def set_focus_time():
+    st.session_state.curr_focus_time = st.session_state.focus_hours * 3600 + st.session_state.focus_minutes * 60
+
+
+def set_break_time():
+    st.session_state.curr_break_time = st.session_state.break_hours * 3600 + st.session_state.break_minutes * 60
+
+
+def format_focus_time(face):
+    hrs = st.session_state.curr_focus_time // 3600
+    mins = (st.session_state.curr_focus_time - hrs * 3600) // 60
+    secs = st.session_state.curr_focus_time - hrs * 3600 - mins * 60
+    face.markdown(f"**{hrs:02d}:{mins:02d}:{secs:02d}**")
+
+
+def format_break_time(face):
+    hrs = st.session_state.curr_break_time // 3600
+    mins = (st.session_state.curr_break_time - hrs * 3600) // 60
+    secs = st.session_state.curr_break_time - hrs * 3600 - mins * 60
+    face.markdown(f"**{hrs:02d}:{mins:02d}:{secs:02d}**")
+
+
+def focus_bttn_toggle():
+    st.session_state.focus_start = not st.session_state.focus_start
+    if st.session_state.focus_label == "Start":
+        st.session_state.focus_label = "Stop"
     else:
-        st.session_state.time_label = "Start"
+        st.session_state.focus_label = "Start"
 
 
-async def timer(face):
+def break_bttn_toggle():
+    st.session_state.break_start = not st.session_state.break_start
+    if st.session_state.break_label == "Start":
+        st.session_state.break_label = "Stop"
+    else:
+        st.session_state.break_label = "Start"
+
+
+async def focus_timer(focus_face):
     while True:
-        st.session_state.curr_time -= 1
-        hrs = st.session_state.curr_time // 3600
-        mins = (st.session_state.curr_time - hrs * 3600) // 60
-        secs = st.session_state.curr_time - hrs * 3600 - mins * 60
-        face.markdown(f"**{hrs:02d}:{mins:02d}:{secs:02d}**")
+        if not st.session_state:
+            break
+        st.session_state.curr_focus_time = max(0, st.session_state.curr_focus_time - 1)
+        format_focus_time(focus_face)
         await asyncio.sleep(1)
 
 
-face = st.empty()
+async def break_timer(break_face):
+    while True:
+        if not st.session_state:
+            break
+        st.session_state.curr_break_time = max(0, st.session_state.curr_break_time - 1)
+        format_break_time(break_face)
+        await asyncio.sleep(1)
+
+
 with timer_col:
-    st.header("Timer")
-    if "timer_start" not in st.session_state:
-        st.session_state.timer_start = False
-    if "time_label" not in st.session_state:
-        st.session_state.time_label = "Start"
-    if "hours" not in st.session_state:
-        st.session_state.hours = 1
-    if "minutes" not in st.session_state:
-        st.session_state.minutes = 30
+    with st.expander("Timer"):
+        st.header("Focus Time")
+        if "focus_start" not in st.session_state:
+            st.session_state.focus_start = False
+        if "focus_label" not in st.session_state:
+            st.session_state.focus_label = "Start"
+        if "focus_hours" not in st.session_state:
+            st.session_state.focus_hours = 1
+        if "focus_minutes" not in st.session_state:
+            st.session_state.focus_minutes = 30
+        if "curr_focus_time" not in st.session_state:
+            st.session_state.curr_focus_time = 0
+            set_focus_time()
 
-    hrs_col, mins_col = st.columns(2)
-    with hrs_col:
-        hours = st.number_input("Hours", key="hours", min_value=0, max_value=24, on_change=set_time)
-    with mins_col:
-        minutes = st.number_input("Minutes", key="minutes", min_value=0, max_value=60, on_change=set_time)
-    timer_col, face_col = st.columns(2)
-    with timer_col:
-        timer_button = st.button(label=st.session_state.time_label, on_click=bttn_toggle)
+        hrs_col, mins_col = st.columns(2)
+        with hrs_col:
+            focus_hours = st.number_input("Hours", key="focus_hours",
+                                          min_value=0, max_value=24, on_change=set_focus_time)
+        with mins_col:
+            focus_minutes = st.number_input("Minutes", key="focus_minutes",
+                                            min_value=0, max_value=60, on_change=set_focus_time)
 
-    if st.session_state.timer_start:
-        st.write("Timer started")
-        asyncio.run(timer(face))
-    else:
-        st.write("Timer stopped")
+        timer_col, reset_col, face_col = st.columns([0.25, 0.25, 0.5])
+        with timer_col:
+            focus_button = st.button(label=st.session_state.focus_label, key="focus", on_click=focus_bttn_toggle)
+        with reset_col:
+            reset_button = st.button(label="Reset", key="focus_reset", on_click=reset_focus_time)
+        with face_col:
+            face = st.empty()
+            format_focus_time(face)
 
+        if st.session_state.focus_start:
+            print("Timer started")
+            asyncio.run(focus_timer(face))
+        else:
+            print("Timer stopped")
+
+        st.divider()
+
+        st.header("Break Time")
+        if "break_start" not in st.session_state:
+            st.session_state.break_start = False
+        if "break_label" not in st.session_state:
+            st.session_state.break_label = "Start"
+        if "break_hours" not in st.session_state:
+            st.session_state.break_hours = 0
+        if "break_minutes" not in st.session_state:
+            st.session_state.break_minutes = 5
+        if "curr_break_time" not in st.session_state:
+            st.session_state.curr_break_time = 0
+            set_break_time()
+
+        hrs_col, mins_col = st.columns(2)
+        with hrs_col:
+            break_hours = st.number_input("Hours", key="break_hours",
+                                          min_value=0, max_value=24, on_change=set_break_time)
+        with mins_col:
+            break_minutes = st.number_input("Minutes", key="break_minutes",
+                                            min_value=0, max_value=60, on_change=set_break_time)
+        timer_col, reset_col, face_col = st.columns([0.25, 0.25, 0.5])
+        with timer_col:
+            break_button = st.button(label=st.session_state.break_label, key="break", on_click=break_bttn_toggle)
+        with reset_col:
+            reset_button = st.button(label="Reset", key="break_reset", on_click=reset_break_time)
+        with face_col:
+            face = st.empty()
+            format_break_time(face)
+
+        if st.session_state.break_start:
+            print("Timer started")
+            asyncio.run(break_timer(face))
+        else:
+            print("Timer stopped")
 # st.write(st.session_state)
