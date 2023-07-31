e379729f2446291393272a60b24ff3a3e66136bf
Javier Perez <javierp621194@gmail.com>
2023-07-31 15:32:28 -0700

    Added page description and pomodoro explanation to main page expanded tabs.

--- pages/Read_Radar.py
+++ pages/Read_Radar.py
@@ -13,9 +13,27 @@ PAGE_TITLE = FILE_NAME.split(".")[0].replace("_", " ")
 st.title(PAGE_TITLE)
 
 with st.expander("Overview", expanded=True):
-    st.write("Description")
+    st.write("This page provides a pomodoro style timer in the sidebar with a built-in pdf viewer that allows you to "
+             "manage your productivity while reading. An explanation of the Pomodoro technique is included below and "
+             "default timer values are already set, you just press the start button and the time will automatically "
+             "count down as you focus on reading.")
 with st.expander("Pomodoro Technique", expanded=False):
-    st.write("Description")
+    st.markdown("From the Wikipedia article for the "
+                "[Pomodoro Technique](https://en.wikipedia.org/wiki/Pomodoro_Technique), "
+                "the Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. "
+                "It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by "
+                "short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the "
+                "tomato-shaped kitchen timer Cirillo used as a university student.")
+    st.markdown("""
+    The original technique has six steps:
+    1. Decide on the task to be done.
+    2. Set the pomodoro timer (traditionally to 25 minutes).
+    3. Work on the task.
+    4. End work when the timer rings and take a short break (typically 5-10 minutes). 
+    5. Go back to Step 2 and repeat until you complete four pomodoros.
+    6. After four pomodoros are done, take a long break (typically 20 to 30 minutes) instead of a short break. Once the
+    long break is finished return to step 2. 
+    """)
 
 st.divider()
 
@@ -73,8 +91,6 @@ def reset_time(mode):
 
 
 def reset_count():
-    st.session_state.pomodoro["count"] = 0
-    st.session_state.pomodoro["focus_count"] = 0
     st.session_state["pomodoro"]["count"] = 0
     st.session_state["pomodoro"]["focus_count"] = 0
 
